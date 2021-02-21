1602ecf715095249c2fc7fa884590a0a0d000a6a
chandai <guy1nthecha1r2049@gmail.com>
2021-02-21 23:50:02 +0900

    add medium:string_to_integer

---
+++ medium/string_to_integer/src/solution.py
@@ -0,0 +1,30 @@
+import re
+
+
+class Solution:
+
+    def myAtoi(self, s: str) -> int:
+        white_space_ignored = s.replace(' ', '')
+        composite_nums = ''
+
+        if white_space_ignored[0] == '-':
+            if not white_space_ignored[1].isdigit():
+                return 0
+            composite_nums = white_space_ignored[0::]
+        elif white_space_ignored[0] == '+':
+            if not white_space_ignored[1].isdigit():
+                return 0
+            composite_nums = white_space_ignored[1::]
+        elif not white_space_ignored[0].isdigit():
+            return 0
+        else:
+            composite_nums = white_space_ignored
+
+        only_nums = float(re.sub('[^\d -.]', '', composite_nums))
+
+        if only_nums < -1 * 2 ** 31:
+            return -1 * 2 ** 31
+        elif (only_nums >= -1 * 2 ** 31) and (only_nums <= (2 ** 31 - 1)):
+            return int(only_nums)
+        else:
+            return 2 ** 31 - 1
---
+++ medium/string_to_integer/test/test_solution.py
@@ -0,0 +1,50 @@
+import unittest
+
+from medium.string_to_integer.src.solution import Solution
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_normal_numeric(self):
+        solution = Solution()
+        expected = 42
+        actual = solution.myAtoi("42")
+
+        self.assertEqual(expected, actual)
+
+    def test_prefix_white_space(self):
+        solution = Solution()
+        expected = -42
+        actual = solution.myAtoi("   -42")
+
+        self.assertEqual(expected, actual)
+
+    def test_suffix_non_digits(self):
+        solution = Solution()
+        expected = 4193
+        actual = solution.myAtoi("4193 with words")
+
+        self.assertEqual(expected, actual)
+
+    def test_prefix_non_digits(self):
+        solution = Solution()
+        expected = 0
+        actual = solution.myAtoi("words and 987")
+
+        self.assertEqual(expected, actual)
+
+    def test_normal_decimal(self):
+        solution = Solution()
+        expected = 3
+        actual = solution.myAtoi("3.14159")
+        self.assertEqual(expected, actual)
+
+
+    def test_composite(self):
+        solution = Solution()
+        expected = 0
+        actual = solution.myAtoi("+-12")
+
+        self.assertEqual(expected, actual)
+if __name__ == '__main__':
+    unittest.main()
