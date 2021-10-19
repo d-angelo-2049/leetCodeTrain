daac73763c1b441b7274e265bf8d2a44bff7d36c
chandai <guy1nthecha1r2049@gmail.com>
2021-10-20 00:40:35 +0900

    add prefix mru queue

--- medium/mru_queue/src/mru_queue.py
+++ medium/mru_queue/src/mru_queue.py
@@ -2,11 +2,11 @@ class MruQueue:
 
     def __init__(self, n):
         self.n = n
-        self.queue = list(range(1, n + 1))
+        self.__queue = list(range(1, n + 1))
 
     def fetch(self, k) -> int:
         index = k-1
-        value = self.queue[index]
-        self.queue.pop(index)
-        self.queue.append(value)
+        value = self.__queue[index]
+        self.__queue.pop(index)
+        self.__queue.append(value)
         return value
